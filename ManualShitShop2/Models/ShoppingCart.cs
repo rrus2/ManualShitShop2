47761934adde16ae0987e2b1169bdf96a4a69969
Pavel <pavel.ivanko@hotmail.com>
2020-02-18 18:44:34 +0100

    AddedShoppingCartLayoutAndReworkButtons

---
+++ ManualShitShop2/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManualShitShop2.Models
+{
+    public class ShoppingCart
+    {
+        public int ID { get; set; }
+        public int ProductID { get; set; }
+        public string UserID { get; set; }
+        public int Amount { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
