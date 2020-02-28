1902f6a42c8fd7810a70f5dc9a871b0c64a45e03
Pavel <pavel.ivanko@hotmail.com>
2020-02-28 13:56:02 +0100

    Finally fixed bullshit with nuget

--- ManualShitShop2.Tests/ProductsTestMoq.cs
+++ ManualShitShop2.Tests/ProductsTestMoq.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using ManualShitShop2.Models;
+using Microsoft.EntityFrameworkCore;
 using Moq;
+using Xunit;
 
 namespace ManualShitShop2.Tests
 {
     public class ProductsTestMoq
     {
+        private readonly ApplicationDbContext _context;
         public ProductsTestMoq()
         {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            _context = new ApplicationDbContext(options);
+            _context.Database.EnsureCreated();
+        }
 
+        [Theory]
+        [InlineData("AK-47", 2000, 20)]
+        public void CreateProductSuccess(string name, decimal price, int stock)
+        {
+            //act
+            var product = new Product { Name = name, Price = price, Stock = stock };
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            //arrange
+            var list = _context.Products.ToList();
+
+            //assert
+            Assert.NotNull(list);
+        }
+
+        private void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
         }
     }
 }
